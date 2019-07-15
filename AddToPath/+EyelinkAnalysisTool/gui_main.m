74bf4abadf45a8ecae8ab8b88539b3c4fd59938b
kmstubbs <kmstubbs@gmail.com>
2019-07-15 15:59:52 -0400

    Update gui_main.m

--- AddToPath/+EyelinkAnalysisTool/gui_main.m
+++ AddToPath/+EyelinkAnalysisTool/gui_main.m
@@ -1,3 +1,20 @@
 function gui_main
 
-fig = open([EyelinkAnalysisTool.getpath 'gui_main.fig']);
+%% Global
+global EATGlobal;
+
+%% Clear Prior
+%close existing figure if there is one
+if isfield(EATGlobal, 'fig_main') && isfield(EATGlobal.fig_main, 'fig') && ishandle(EATGlobal.fig_main.fig)
+    close(EATGlobal.fig_main.fig);
+end
+
+%clear old figure data
+EATGlobal.fig_main = [];
+
+%% Open New
+%open new figure
+EATGlobal.fig_main.fig = open([EyelinkAnalysisTool.getpath 'gui_main.fig']);
+
+%% Initialize
+%TODO
